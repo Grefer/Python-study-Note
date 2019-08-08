a8170c0301211df9ac82568629488e3497c8664c
Minevei <53202369+Minevei@users.noreply.github.com>
2019-08-08 13:38:15 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -788,3 +788,25 @@ print(s.name)         #实例属性优先级高于类属性
 print(Student.name)
 del s.name
 print(s.name)
+
+
+class Student(object):
+    count=0
+    def __init__(self,name):
+        self.name=name
+        Student.count+=1      #每创建一个实例，该属性+1
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
