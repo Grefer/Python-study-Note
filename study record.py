03e8f213eec1009a9db04a3586a4c44b4e0ad0b7
Minevei <53202369+Minevei@users.noreply.github.com>
2019-08-15 17:26:01 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -1068,7 +1068,36 @@ callable(Student())
 callable([1,2,3])
 callable(s)             #加入__call__的类实例可调用
 
+'''使用枚举类'''
+from enum import Enum
+Month = Enum('Month',('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'))
 
+for name,member in Month.__members__.items():
+    print(name,'=>',member,',',member.value)
+
+from enum import Enum,unique
+
+@unique
+class Weekday(Enum):
+    Sun=0
+    Mon=1
+    Tue=2
+    Wed=3
+    Thr=4
+    Fri=5
+    Sat=6
+
+day1=Weekday.Mon
+print(day1)
+print(Weekday.Tue)
+print(Weekday['Tue'])
+print(Weekday.Tue.value)
+print(day1==Weekday.Mon)
+print(day1==Weekday.Tue)
+print(day1==Weekday(1))
+Weekday(7)
+for name,member in Weekday.__members__.items():
+    print(name,'=>',member)
 
 
 
