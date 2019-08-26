50718a3378f586bebd8e0bb1b94ac9642dbed3c9
Minevei <53202369+Minevei@users.noreply.github.com>
2019-08-26 11:21:28 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -1593,5 +1593,27 @@ f=StringIO()
 f.write('hello')
 f.write(' ')
 f.write('world!')
+print(f.getvalue())     #getvalue()方法用于获得写入后的str
+
+from io import StringIO
+f=StringIO('Hello!\nHi!\nGoodbye!')
+while True:
+    s=f.readline()
+    if s == '':
+        break
+    print(s.strip())
+
+'''BytesIO'''
+from io import BytesIO
+f=BytesIO()
+f.write('中文'.encode('utf-8'))
 print(f.getvalue())
+
+from io import BytesIO
+f=BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+f.read()
+
+
+
+    
 
