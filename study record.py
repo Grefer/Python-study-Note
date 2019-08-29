04472e2eedfcd343c2402ff71e32c4166ff34c28
Minevei <53202369+Minevei@users.noreply.github.com>
2019-08-29 17:30:08 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -1715,3 +1715,55 @@ obj= dict(name='小明',age=23)
 s = json.dumps(obj,ensure_ascii=False)
 s
 #ensure_ascii=True时，中文被转换为ASCII码，ensure_ascii=False时，中文不转换
+
+'''进程和线程'''
+#对于操作系统来说，一个任务就是一个进程（Process）,进程内的这些“子任务”称为线程（Thread）
+
+'''多进程'''
+import os
+print('Process (%s) start...' %os.getpid())
+pid=os.fork()      #Only works on Unix/Linux/Mac:
+if pid == 0:
+    print('I am child process (%s) of father process %s.'%(os.getpid()) %os.getppid)
+else:
+    print('I (%s) just created a child process (%s).' %(os.getpid(),pid))
+
+#Multiprocessing
+from multiprocessing import Process
+import os
+
+#子进程执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...'%(name,os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.'%os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+
+#pool
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
+
+    
+
