001165c058a5df760b8c680043677306b1c4da5f
Minevei <53202369+Minevei@users.noreply.github.com>
2019-09-01 14:13:05 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -1812,3 +1812,88 @@ if __name__=='__main__':
     pw.join()
     # pr进程里是死循环，无法等待其结束，只能强行终止:
     pr.terminate()
+
+'''多线程'''
+import time ,threading
+
+#新线程执行的代码:
+def loop():
+    print('Thread %s is running...' %(threading.current_thread()).name)
+    n=0
+    while n <5:
+        n=n+1
+        print('thread %s >>> %s' %(threading.current_thread().name,n))
+        time.sleep(1)
+    print('thread %s ended.' %threading.current_thread().name)
+
+print('thread %s is running...' %threading.current_thread().name)
+t=threading.Thread(target=loop,name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)        
+
+#Lock
+
+# 假定这是你的银行存款
+balance = 0
+def change_it(n):
+    #先存后取，结果为0
+    global balance
+    balance = balance + n
+    balance = balance - n
+def run_thread(n):
+    for i in range(1000000):   #循环次数足够多时，balance值!=0
+        change_it(n)
+t1 = threading.Thread(target=run_thread,args=(5,))
+t2 = threading.Thread(target=run_thread,args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)   #多线程同时对balance进行操作，容易改乱数值
+
+#使用lock保证balance同时只能被一个线程操作
+balance = 0
+lock = threading.Lock()
+def run_thread(n):
+    for i in range(1000000):
+        #先要获取锁
+        lock.acquire()
+        try:
+            #放心地改吧
+            change_it(n)
+        finally:
+            #改完之后一定要释放锁!!!!!
+            lock.release()
+t1 = threading.Thread(target=run_thread,args=(5,))
+t2 = threading.Thread(target=run_thread,args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
+
+#多核CPU
+import threading,multiprocessing
+def loop():
+    x=0
+    while True:
+        x = x ^ 1
+for i in range(multiprocessing.cpu_count()):
+    t = threading.Thread(target=loop)
+    t.start()    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
