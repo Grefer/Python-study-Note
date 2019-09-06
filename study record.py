351c7165e0d9b46fd7291e795dad8eb8f47bd79e
Minevei <53202369+Minevei@users.noreply.github.com>
2019-09-06 17:20:33 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -2108,7 +2108,20 @@ tk2_dt = bj_dt.astimezone(timezone(timedelta(hours=9)))
 print(tk2_dt)
 
 #练习
+import re
 def to_timestamp(dt_str,tz_str):
-    
+    str_dt=datetime.strptime(dt_str,'%Y-%m-%d %H:%M:%S')  #str转时间
+    tz=re.match(r'^(UTC)([+|-]\d+):00$',tz_str)           #时区str分组 
+    h=int(tz.group(2))                                    #取出时区组并转为int
+    utc_dt=str_dt.replace(tzinfo=timezone(timedelta(hours=h)))  #设定时区
+    print(utc_dt)
+    return utc_dt.timestamp()                             #datetime转化为stamp
 
- 
+# 测试:
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
+
+print('ok') 
