0f01b94a58e921309eda4191fdc913a568339fdb
Minevei <53202369+Minevei@users.noreply.github.com>
2019-09-19 17:21:40 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -2462,3 +2462,70 @@ def closing(thing):
         yield thing
     finally:
         thing.close()
+
+'''urllib'''
+from urllib import request
+
+with request.urlopen('http://news-at.zhihu.com/api/4/news/latest') as f:
+    data=f.read()
+    print('Status:',f.status,f.reason)
+    for k,v in f.getheaders():
+        print('%s: %s'%(k,v))
+    print('Data:',data.decode('utf-8'))
+
+req=request.Request('http://www.douban.com/')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
+    
+#Post
+from urllib import parse
+
+print('Login to weibo.cn...')
+phone=input('Phone No.:')
+passwd=input('Password:')
+login_data=parse.urlencode([
+        ('username',phone),
+        ('password',passwd),
+        ('entry','mweibo'),
+        ('client_id',''),
+        ('savestate','1'),
+        ('ec',''),
+        ('pagerefer','https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+        ])
+req=request.Request('https://passport.weibo.cn/sso/login')    
+req.add_header('Origin', 'https://passport.weibo.cn')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+
+with request.urlopen(req,data=login_data.encode('utf-8')) as f:
+    print('Status:',f.status,f.reason)
+    for k,v in f.getheaders():
+        print('%s:%s' %(k,v))
+    print('Data:',f.read().decode('utf-8'))
+
+#Handler   
+#proxy_handler = urllib.request.ProxyHandler({'http': 'http://www.example.com:3128/'})
+#proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()
+#proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+#opener = urllib.request.build_opener(proxy_handler, proxy_auth_handler)
+#with opener.open('http://www.example.com/login.html') as f:
+#    pass
+
+#练习
+import json
+from urllib import request
+def fetch_data(url):
+    req=request.Request(url)
+    with request.urlopen(req) as f:
+        print('Status:',f.status,f.reason)
+        data=f.read()
+        return json.loads(data.decode('utf-8'))
+
+# 测试
+URL = 'http://news-at.zhihu.com/api/4/news/latest'
+Data=fetch_data(URL)
+print(Data)
