4590a64ba4b99bb938ca9e4ed91d2a5fcce0c0ae
Minevei <53202369+Minevei@users.noreply.github.com>
2019-10-12 16:13:25 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -3074,4 +3074,31 @@ for data in [b'Michael', b'Tracy', b'Sarah']:
     print(s.recv(1024).decode('utf-8'))
 s.send(b'exit')
 s.close()
+
+'''UDP编程'''
+import socket
+#SOCK_DGRAM指定了这个Socket的类型是UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定端口:
+s.bind(('127.0.0.1', 9999))
+
+print('Bind UDP on 9999...')
+while True:
+    # 接收数据:
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    s.sendto(b'Hello, %s!' % data, addr)
+
+#客户端
+import socket    
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.sendto(data, ('127.0.0.1', 9999))
+    # 接收数据:
+    print(s.recv(1024).decode('utf-8'))
+s.close()
+
+
+
     
