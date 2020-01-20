e7143f7f5f86adeab084f400aba58b1678e9186b
Minevei <53202369+Minevei@users.noreply.github.com>
2020-01-20 15:16:53 +0800

    Update study record.py

--- study record.py
+++ study record.py
@@ -3315,7 +3315,7 @@ K=4.1       #exercise rate*(%)
 r= 2.55     #risk-free rate(%)
 sigma=20     #valatility rate(%)
 t=0.25         #option start time
-T=1       #option term(Y)
+T=1         #option term(Y)
 
 d1=(math.log(F/K)+math.pow(sigma/100,2)*t/2)/(sigma/100*math.sqrt(t))
 d2=d1-sigma/100*math.sqrt(t)
@@ -3327,7 +3327,7 @@ print('d1=%.4f,d2=%.4f'%(d1,d2))
 print('The caplet price rate is:%.2f BP,and the floorlets price rate is:%.2f BP'%(c,f))
 print('The caplet price is:%.2f,and the floorlet price is:%.2f'%(c*L*100,f*L*10000))
 
-                
+input()          
 
 
    
