2490d3fa1a71e5999dcb3ddecbbe51f824d48298
Grefer <53202369+Grefer@users.noreply.github.com>
2020-11-21 14:23:21 +0800

    Update GARCH-Lap-Grefer.py

--- GARCH-Lap-Grefer.py
+++ GARCH-Lap-Grefer.py
@@ -13,32 +13,32 @@ import matplotlib.pyplot as plt
 import datetime
 import arch
 
-SA = pd.read_csv(open(r"C:\Users\Admin\Desktop\OneDrive\Project B\Convertable Bond\南航行情序列.csv"))    #导入时间序列
+SA = pd.read_csv(open(r"D:\Desktop\One Driver\OneDrive\Project B\Convertable Bond\南航行情序列.csv"))    #导入时间序列
 
 SA.columns =['date','price']
 SA['date'] = pd.to_datetime(SA['date'])
 SA = SA.set_index('date')            #指定日期为索引值方便调用
 
-SA['return'] = np.log(SA['price']/SA['price'].shift(1))
+SA['return'] = np.log(SA['price']/SA['price'].shift(1))     #计算连续收益率
+SA['vol']=SA['return'].rolling(window=5).std()         #以周为窗口计算滚动标准差/日波动率
 
-SA[['price','return']].plot(subplots = True,style = 'b',figsize=(8,5))
-
-
+SA[['price','return','vol']].plot(subplots = True,style = 'b',figsize=(8,5))
 
 sample = SA['2019-10-14':'2020-06-14']    #指定样本集
 test = SA['2020-06-15':'2020-10-14']      #指定测试集
 
-vol = np.sqrt(np.sum(np.square(SA['return']-np.average(SA['return']))) / sample.size)
-annualizedVol = vol * np.sqrt(252)
-
 
 #GARCH模型建立
 
-model = arch.arch_model(sample, mean='AR', vol='GARCH', p=1, q=1)
+model = arch.arch_model(sample['return']*100, mean='AR', vol='GARCH',p=1,q=1)
+model_fit = model.fit(update_freq=0)
+print(model_fit.summary())
 
-model_fit = model.fit()
 
-yhat = model_fit.forecast(horizon=164)
+for i in test['vol']:
+    
+    f_vol = model_fit.forecast(horizon=5)
+
 
 var = [i*0.01 for i in range(0,100)]
 plt.plot(var[163:])
