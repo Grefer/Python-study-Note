8385dc4cd8d98fb99a346a6f63e6b10fcc212e90
Grefer <53202369+Grefer@users.noreply.github.com>
2020-11-22 01:38:08 +0800

    Update GARCH-Lap-Grefer.py

--- GARCH-Lap-Grefer.py
+++ GARCH-Lap-Grefer.py
@@ -19,29 +19,27 @@ SA.columns =['date','price']
 SA['date'] = pd.to_datetime(SA['date'])
 SA = SA.set_index('date')            #指定日期为索引值方便调用
 
-SA['return'] = np.log(SA['price']/SA['price'].shift(1))     #计算连续收益率
-SA['vol']=SA['return'].rolling(window=5).std()         #以周为窗口计算滚动标准差/日波动率
+SA['return'] = np.log(SA['price']/SA['price'].shift(1))      #计算连续收益率%
+SA['vol']=SA['return'].rolling(window=5).std()          #计算滚动标准差/日波动率
 
-SA[['price','return','vol']].plot(subplots = True,style = 'b',figsize=(8,5))
+#SA[['price','return','vol']].plot(subplots = True,style = 'b',figsize=(8,5))
 
-sample = SA['2019-10-14':'2020-06-14']    #指定样本集
-test = SA['2020-06-15':'2020-10-14']      #指定测试集
+split_date = '2020-06-15'
+sample = SA['2019-10-14':split_date]    #指定样本集
+test = SA[split_date:'2020-10-14']      #指定测试集
 
 
 #GARCH模型建立
 
-model = arch.arch_model(sample['return']*100, mean='AR', vol='GARCH',p=1,q=1)
-model_fit = model.fit(update_freq=0)
+model = arch.arch_model(sample['return'], mean='AR', vol='GARCH',p=1,q=1,dist='StudentsT')
+model_fit = model.fit(last_obs=split_date)
 print(model_fit.summary())
 
 
-for i in test['vol']:
-    
-    f_vol = model_fit.forecast(horizon=5)
+f_vol = model_fit.forecast()
+      
 
-
-var = [i*0.01 for i in range(0,100)]
-plt.plot(var[163:])
-# plot forecast variance
-plt.plot(yhat.variance.values[-1, :])
+np.sqrt(f_vol.variance[split_date:]).plot()
 plt.show()
+
+SA['vol']
